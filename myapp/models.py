ffdadc4456ce9fef43636e4a8033d4c883acd4e1
Kjeff <jeffreyarthur123@gmail.com>
2022-03-29 00:14:30 +0800

    Created database Models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100, default='My product name')
+    details = models.TextField(default='My products details')
+    img = models.ImageField(upload_to = "products/", null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default='45')
+    
