4a9d87c189ebd8f43d0ab75923120f5917254280
Kjeff <jeffreyarthur123@gmail.com>
2022-06-03 09:57:28 +0000

    added static files

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(0n@m7*#lrt33hw!60a4dj@seb8j#(qm&mr$9=p!e-rzlrm^4r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.78.183', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.78.183', '127.0.0.1', 'wellnesspharma.herokuapp.com']
 #'127.0.0.1', 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 
